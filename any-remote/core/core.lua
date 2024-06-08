aa2f31fea6c34a16e0793f4730e5bbd843af0a05
nekit508 <lastp508@gmail.com>
2024-06-08 17:14:35 +0400

    this is test commit

--- any-remote/core/core.lua
+++ any-remote/core/core.lua
@@ -2,10 +2,10 @@ G.info("Current root is " .. G.updater.root:absolutePath())
 G.info("Start files processing? Y/n")
 
 function process()
-
+    G.info("processing")
 end
 
 resp = G.waitLineInput();
 if resp == "y" or resp == "Y" then
-
+    process()
 end
